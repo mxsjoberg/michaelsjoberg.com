0f851e1e75e007589c61a5c00887da569a135af1
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-04-29 16:14:54 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -73,6 +73,15 @@ class PagesController < ApplicationController
         @meta_title = "Writing – michael sjöberg"
         # params
         @post = params[:post]
+        # posts.json
+        @posts = JSON.parse(File.read(Rails.public_path + 'posts.json'))
+        # random
+        if @post == "random"
+            # pick random sample in posts.json
+            @post = @posts.keys.sample
+            # redirect
+            redirect_to "/writing/#{@post}"
+        end
         # redirects
         if @post == "a-few-notes-on-investing" || @post == "investing-in-stocks-like-a-pro" || @post == "how-to-invest-in-stocks-like-a-pro"
             @post = "how-to-invest-in-stocks"
@@ -86,51 +95,90 @@ class PagesController < ApplicationController
         if @post == "computer-systems-and-low-level-software-security" || @post == "a-computer-systems-primer-for-application-developers"
             @post = "lecture-notes-on-security-engineering"
         end
-        # posts.json
-        @posts = JSON.parse(File.read(Rails.public_path + 'posts.json'))
-        # post
+        # render post
         unless (@post.nil?)
-            @file = @post + '.md'
-            @title = @posts[@post]['title']
-            # @short = @posts[@post]['short']
-            # @note = @posts[@post]['note']
-            @tags = @posts[@post]['tags']
-            @date = @posts[@post]['date']
-            @updated = @posts[@post]['updated']
-            @draft = @posts[@post]['draft']
-            @toc = @posts[@post]['toc']
-            # @intro = @posts[@post]['intro']
-            # @image = @posts[@post]['image']
-            @all_lines = File.read(Rails.public_path + 'posts/' + @file)
-            @lines = File.readlines(Rails.public_path + 'posts/' + @file)
-            # override meta title
-            @meta_title = @title + " | Michael Sjöberg"
-            # count words
-            @words = 0
-            @lines.drop(5).each do |line|
-                words = line.split(' ')
-                words.each do |word|
-                    @words += 1
+            # tag
+            if @post == "algorithms" || @post == "computer-science" || @post == "machine-learning" || @post == "finance" || @post == "misc" || @post == "projects"
+                @tag = @post
+                # filter posts.json by tag
+                @posts_array = Hash.new
+                @posts.keys.each do |post|
+                    if @posts[post]['tags'].include? @post
+                        @title = @posts[post]['title']
+                        @tags = @posts[post]['tags']
+                        @date = @posts[post]['date']
+                        @updated = @posts[post]['updated']
+                        @draft = @posts[post]['draft']
+                        if @draft
+                            # set date to this year, format YYYYMMDD
+                            @date = Time.now.strftime("%Y%m%d")
+                            @updated = @date
+                        end
+                        @read = @posts[post]['read']
+                        @intro = @posts[post]['intro']
+                        @posts_array[post] = {
+                            title: @title,
+                            tags: @tags,
+                            date: @date,
+                            updated: @updated,
+                            draft: @draft,
+                            read: @read,
+                            intro: @intro
+                        }
+                    end
+                end
+                # set post to nil
+                @post = nil
+                # sort by date if not sorted already
+                @posts_array = @posts_array.sort_by{ |_,h| -h[:date].to_i }.to_h
+                # override meta title
+                @meta_title = "Computer Science – michael sjöberg"
+            else
+                @file = @post + '.md'
+                @title = @posts[@post]['title']
+                @tags = @posts[@post]['tags']
+                @date = @posts[@post]['date']
+                @updated = @posts[@post]['updated']
+                @draft = @posts[@post]['draft']
+                if @draft
+                    # set date to this year, format YYYYMMDD
+                    @date = Time.now.strftime("%Y%m%d")
+                    @updated = @date
+                end
+                @toc = @posts[@post]['toc']
+                @all_lines = File.read(Rails.public_path + 'posts/' + @file)
+                @lines = File.readlines(Rails.public_path + 'posts/' + @file)
+                # override meta title
+                @meta_title = @title + " | Michael Sjöberg"
+                # count words
+                @words = 0
+                @lines.drop(5).each do |line|
+                    words = line.split(' ')
+                    words.each do |word|
+                        @words += 1
+                    end
                 end
             end
-        # render all posts
+        # render all posts unless hash already created by filter
         else
+            @tags_all = Array.new
             @posts_array = Hash.new
             @posts.keys.each do |post|
                 @file = post + '.md'
                 @title = @posts[post]['title']
-                # @short = @posts[post]['short']
-                # @note = @posts[post]['note']
                 @tags = @posts[post]['tags']
                 @date = @posts[post]['date']
                 @updated = @posts[post]['updated']
                 @draft = @posts[post]['draft']
+                if @draft
+                    # set date to this year, format YYYYMMDD
+                    @date = Time.now.strftime("%Y%m%d")
+                    @updated = @date
+                end
                 @read = @posts[post]['read']
                 @intro = @posts[post]['intro']
                 @posts_array[post] = {
                     title: @title,
-                    # short: @short,
-                    # note: @note,
                     tags: @tags,
                     date: @date,
                     updated: @updated,
@@ -138,9 +186,17 @@ class PagesController < ApplicationController
                     read: @read,
                     intro: @intro
                 }
+                # add tags to array
+                @tags.each do |tag|
+                    @tags_all.push(tag)
+                end
             end
             # sort by date if not sorted already
             @posts_array = @posts_array.sort_by{ |_,h| -h[:date].to_i }.to_h
+            # remove duplicate in tags_array
+            @tags_all = @tags_all.uniq!
+            # sort tags_all
+            @tags_all = @tags_all.sort
         end
     end
 
