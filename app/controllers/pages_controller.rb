bde7b5c590148f69aa75c4ec93dee21046923624
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-05-16 22:29:27 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
     before_action :set_meta
 
     # version
-    $version = "4.6.0"
+    $version = "5.0.0"
 
     # GET /
     def home
         @route_path = "home"
-        @meta_title = "michael sjöberg"
+        @meta_title = "Michael Sjöberg"
         # posts.json
         @posts = JSON.parse(File.read(Rails.public_path + 'posts.json'))
         @posts_array = Hash.new
@@ -27,7 +27,7 @@ class PagesController < ApplicationController
     # GET /programming
     def programming
         @route_path = "programming"
-        @meta_title = "Programming – michael sjöberg"
+        @meta_title = "Programming"
         # params
         @category = params[:category]
         @group = params[:group]
@@ -52,7 +52,7 @@ class PagesController < ApplicationController
     # GET /finance
     def finance
         @route_path = "finance"
-        @meta_title = "Finance – michael sjöberg"
+        @meta_title = "Finance"
         # finance.json
         @finance = JSON.parse(File.read(Rails.public_path + 'finance.json'))
         @url = @finance["url"]
@@ -62,7 +62,7 @@ class PagesController < ApplicationController
     # GET /projects
     def projects
         @route_path = "projects"
-        @meta_title = "Projects – michael sjöberg"
+        @meta_title = "Projects"
         # projects.json
         @projects = JSON.parse(File.read(Rails.public_path + 'projects.json'))
     end
@@ -70,7 +70,7 @@ class PagesController < ApplicationController
     # GET /writing
     def writing
         @route_path = "writing"
-        @meta_title = "Writing – michael sjöberg"
+        @meta_title = "Writing"
         # params
         @post = params[:post]
         # posts.json
@@ -132,7 +132,7 @@ class PagesController < ApplicationController
                 # sort by date if not sorted already
                 @posts_array = @posts_array.sort_by{ |_,h| -h[:date].to_i }.to_h
                 # override meta title
-                @meta_title = "Computer Science – michael sjöberg"
+                @meta_title = "Computer Science"
             else
                 @file = @post + '.md'
                 @title = @posts[@post]['title']
@@ -203,7 +203,7 @@ class PagesController < ApplicationController
     # GET /about/courses
     def courses
         @route_path = "courses"
-        @meta_title = "Course List – michael sjöberg"
+        @meta_title = "Course List"
         # courses
         @courses = JSON.parse(File.read(Rails.public_path + 'courses.json'))
     end
@@ -214,6 +214,6 @@ class PagesController < ApplicationController
             @meta_image = ""
             @meta_site_name = "michaelsjoeberg.com"
             @meta_card_type = "summary"
-            @meta_author = "@sjoebergco"
+            @meta_author = "@miqqeio"
         end
 end
