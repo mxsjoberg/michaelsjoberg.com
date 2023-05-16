69d296ed50a06faafe0f869b860865524dd4abc2
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-05-16 22:39:20 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   before_action :set_meta
-  # version
   $version = "5.0.0"
+  # ----------------------------------------------
   # GET /
+  # ----------------------------------------------
   def home
     @route_path = "home"
     @meta_title = "Michael Sjöberg"
@@ -21,7 +22,9 @@ class PagesController < ApplicationController
     # sort by date if not sorted already
     @posts_array = @posts_array.sort_by{ |_,h| -h[:date].to_i }.to_h
   end
+  # ----------------------------------------------
   # GET /programming
+  # ----------------------------------------------
   def programming
     @route_path = "programming"
     @meta_title = "Programming"
@@ -45,7 +48,9 @@ class PagesController < ApplicationController
       end
     end
   end
+  # ----------------------------------------------
   # GET /finance
+  # ----------------------------------------------
   # def finance
   #   @route_path = "finance"
   #   @meta_title = "Finance"
@@ -54,14 +59,18 @@ class PagesController < ApplicationController
   #   @url = @finance["url"]
   #   @format = @finance["format"]
   # end
+  # ----------------------------------------------
   # GET /projects
+  # ----------------------------------------------
   def projects
     @route_path = "projects"
     @meta_title = "Projects"
     # projects.json
     @projects = JSON.parse(File.read(Rails.public_path + 'projects.json'))
   end
+  # ----------------------------------------------
   # GET /writing
+  # ----------------------------------------------
   def writing
     @route_path = "writing"
     @meta_title = "Writing"
@@ -193,14 +202,18 @@ class PagesController < ApplicationController
       @tags_all = @tags_all.sort
     end
   end
+  # ----------------------------------------------
   # GET /about/courses
+  # ----------------------------------------------
   def courses
     @route_path = "courses"
     @meta_title = "Course List"
     # courses
     @courses = JSON.parse(File.read(Rails.public_path + 'courses.json'))
   end
+  # ----------------------------------------------
   # private
+  # ----------------------------------------------
   private
     # meta
     def set_meta
