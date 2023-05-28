c80552e5f6a889526071f3b078f86b190c65c785
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-05-28 15:13:04 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -92,7 +92,6 @@ class PagesController < ApplicationController
       @file = filename.split('/').last
       @images.push(@file)
     end
-    # @images.sort_by(&:to_i)
   end
   # ----------------------------------------------
   # GET /writing
@@ -104,26 +103,6 @@ class PagesController < ApplicationController
     @post = params[:post]
     # posts.json
     @posts = JSON.parse(File.read(Rails.public_path + 'posts.json'))
-    # random
-    # if @post == "random"
-    #   # pick random sample in posts.json
-    #   @post = @posts.keys.sample
-    #   # redirect
-    #   redirect_to "/writing/#{@post}"
-    # end
-    # redirects
-    # if @post == "a-few-notes-on-investing" || @post == "investing-in-stocks-like-a-pro" || @post == "how-to-invest-in-stocks-like-a-pro"
-    #   @post = "how-to-invest-in-stocks"
-    # end
-    # if @post == "computer-vision-in-a-hurry" || @post == "a-deep-dive-into-computer-vision"
-    #   @post = "lecture-notes-on-computer-vision"
-    # end
-    # if @post == "why-are-financial-data-apps-so-bad"
-    #   @post = "building-an-alternative-to-yahoo-finance"
-    # end
-    # if @post == "computer-systems-and-low-level-software-security" || @post == "a-computer-systems-primer-for-application-developers"
-    #   @post = "lecture-notes-on-security-engineering"
-    # end
     # render post
     unless (@post.nil?)
       # tag
@@ -170,19 +149,11 @@ class PagesController < ApplicationController
         @author = @post_details[1].strip
         @date = @post_details[2].strip
         @updated = @post_details[3].strip
-        # @filename = filename.split('/').last.split('.').first
-        # @title = @posts[@post]['title']
-        # @tags = @posts[@post]['tags']
-        # @date = @posts[@post]['date']
-        # @updated = @posts[@post]['updated']
-        # @draft = @posts[@post]['draft']
         if @draft
           # set date to this year, format YYYYMMDD
           @date = Time.now.strftime("%Y%m%d")
           @updated = @date
         end
-        # @toc = @posts[@post]['toc']
-        # @all_lines = File.read(Rails.public_path + 'posts/' + @file)
         @lines = File.readlines(Rails.public_path + 'posts/' + @file).drop(4)
         @all_lines = @lines.join
         # override meta title
@@ -198,47 +169,11 @@ class PagesController < ApplicationController
       end
     # render all posts unless hash already created by filter
     else
-      # @tags_all = Array.new
-      # @posts_array = Hash.new
-      # @posts.keys.each do |post|
-      #   @file = post + '.md'
-      #   @title = @posts[post]['title']
-      #   @tags = @posts[post]['tags']
-      #   @date = @posts[post]['date']
-      #   @updated = @posts[post]['updated']
-      #   @draft = @posts[post]['draft']
-      #   if @draft
-      #     # set date to this year, format YYYYMMDD
-      #     @date = Time.now.strftime("%Y%m%d")
-      #     @updated = @date
-      #   end
-      #   @read = @posts[post]['read']
-      #   @intro = @posts[post]['intro']
-      #   @posts_array[post] = {
-      #     title: @title,
-      #     tags: @tags,
-      #     date: @date,
-      #     updated: @updated,
-      #     draft: @draft,
-      #     read: @read,
-      #     intro: @intro
-      #   }
-      #   # add tags to array
-      #   @tags.each do |tag|
-      #     @tags_all.push(tag)
-      #   end
-      # end
-      # # sort by date if not sorted already
-      # @posts_array = @posts_array.sort_by{ |_,h| -h[:date].to_i }.to_h
-      # # remove duplicate in tags_array
-      # @tags_all = @tags_all.uniq!
-      # # sort tags_all
-      # @tags_all = @tags_all.sort
       @dir_posts = []
       Dir.glob(Rails.public_path + 'posts/*.md') do |filename|
         next if filename.include? 'OLD'
         @post_details = File.readlines(filename).first(4) # array
-        # TODO remove this when all posts are updated
+        # draft
         next if @post_details[0].include? '<'
         # post details
         @title = @post_details[0].strip
