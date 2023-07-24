95f489b19974db2aa6b2a57ac85569be2877e7f9
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-07-24 12:34:05 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
     get "/about/stack"                                  => "pages#stack", as: "stack"
     get "/about/courses"                                => "pages#courses", as: "courses"
     get "/programming"                                  => "pages#programming", as: "programming"
-    get "/programming/:category/:group/:file"           => 'pages#programming', as: "file"
+    get "/programming/:category/:group/:file"           => "pages#programming", as: "file"
     get "/projects"                                     => "pages#projects", as: "projects"
     get "/projects/attefall"                            => "pages#attefall", as: "attefall"
     get "/writing"                                      => "pages#writing", as: "writing"
-    get "/post/:post"                                   => "pages#post", as: "post"
+    get "/posts"                                        => "pages#posts", as: "posts"
+    get "/posts/:post"                                  => "pages#posts", as: "post"
 end
